a3183cccd88263e77984f889c75117c4e9c0a5fb
Clivern <hello@clivern.com>
2020-08-09 18:45:50 +0200

    update

--- src/main/java/com/clivern/GreeterMain.java
+++ src/main/java/com/clivern/GreeterMain.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2020 Clivern <https://clivern.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.clivern;
 
 import akka.actor.typed.ActorRef;
@@ -34,8 +47,7 @@ public class GreeterMain extends AbstractBehavior<GreeterMain.SayHello> {
 
     private Behavior<SayHello> onSayHello(SayHello command) {
         //#create-actors
-        ActorRef<Greeter.Greeted> replyTo =
-                getContext().spawn(GreeterBot.create(3), command.name);
+        ActorRef<Greeter.Greeted> replyTo = getContext().spawn(GreeterBot.create(3), command.name);
         greeter.tell(new Greeter.Greet(command.name, replyTo));
         //#create-actors
         return this;
